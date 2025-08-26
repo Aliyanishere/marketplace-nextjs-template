6a35f4c949c289fb605fd1f7d821d4d3ec8c0b91
Aliyan Hussain <hussainaliyan531@gmail.com>
2025-08-26 16:51:32 +0500

    unused import fix

--- web/components/account/account-list-feature.tsx
+++ web/components/account/account-list-feature.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletButton } from '../solana/solana-provider';
 
 import { redirect } from 'next/navigation';
 
